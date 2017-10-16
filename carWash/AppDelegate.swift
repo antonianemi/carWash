925306aa0755ee37de155f8ba8b768f47b0beee6
antonio de jesus cortes lagunes <antoni_anemi@hotmail.com>
2017-10-15 22:33:22 -0700

    Add Login with Facebook
    [*] Add pod file for Facebook Login.
    [*] Add code to show the button for facebook from login.

--- carWash/AppDelegate.swift
+++ carWash/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        // Add any custom logic here.
         return true
     }
 
@@ -39,6 +42,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        let handled: Bool = FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+        // Add any custom logic here.
+        return handled
     }
 
 
--- carWash/ViewController.swift
+++ carWash/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import FacebookLogin
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let btn_login = LoginButton(readPermissions: [.publicProfile])
+        btn_login.center = view.center
+        view.addSubview(btn_login)
     }
 
     override func didReceiveMemoryWarning() {
